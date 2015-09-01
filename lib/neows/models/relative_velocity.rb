03acb04ff7411b136939d32a56e7cacee7e349a2
David Greenfield <dgreenfield@constantcontact.com>
2015-08-31 22:16:09 -0700

    update rel velocity model to neows update

--- lib/neows/models/relative_velocity.rb
+++ lib/neows/models/relative_velocity.rb
@@ -6,15 +6,15 @@ module Neows
     class RelativeVelocity < Neows::Models::BaseModel
       # @!attribute [rw]
       # @return [Float]
-      attribute :kms, Float
+      attribute :kilometers_per_second, Float
 
       # @!attribute [rw]
       # @return [Float]
-      attribute :kph, Float
+      attribute :kilometers_per_hour, Float
 
       # @!attribute [rw]
       # @return [Float]
-      attribute :mph, Float
+      attribute :miles_per_hour, Float
     end
   end
 end
