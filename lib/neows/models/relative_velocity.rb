74d6f381adf666fb1ef0cbf0483f79e3c9007103
David Greenfield <dgreenfield@constantcontact.com>
2015-08-31 22:36:34 -0700

    Per pull reqReview - Updated with alias attributes and updated spec

--- lib/neows/models/relative_velocity.rb
+++ lib/neows/models/relative_velocity.rb
@@ -7,14 +7,17 @@ module Neows
       # @!attribute [rw]
       # @return [Float]
       attribute :kilometers_per_second, Float
+      alias_method :kps, :kilometers_per_second
 
       # @!attribute [rw]
       # @return [Float]
       attribute :kilometers_per_hour, Float
+      alias_method :kph, :kilometers_per_hour
 
       # @!attribute [rw]
       # @return [Float]
       attribute :miles_per_hour, Float
+      alias_method :mph, :miles_per_hour
     end
   end
 end
--- spec/models/relative_velocity_spec.rb
+++ spec/models/relative_velocity_spec.rb
@@ -4,8 +4,11 @@ describe Neows::Models::RelativeVelocity do
   subject { described_class.new json_fixture('/relative_velocity.json') }
 
   it 'correctly assigns data to attributes' do
-    expect(subject.kms).to eq 6.57260631740581
+    expect(subject.kilometers_per_second).to eq 6.57260631740581
+    expect(subject.kps).to eq 6.57260631740581
+    expect(subject.kilometers_per_hour).to eq 23661.383
     expect(subject.kph).to eq 23661.383
+    expect(subject.miles_per_hour).to eq 14702.263
     expect(subject.mph).to eq 14702.263
   end
 end
